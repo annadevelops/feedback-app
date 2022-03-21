bc12fa316274cfc7f1ecc63f499a376c255aaf8f
Anna N <info@annadevelops.com>
2022-03-21 11:57:14 +0000

    Connect Routes to the application - now if goes to /about the About page is rendered

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4} from 'uuid'; // Import the components and data files for UUID which generates unique ID for objects
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
@@ -6,6 +7,7 @@ import { useState } from "react";
 
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from './pages/AboutPage';
 
 function App() {
 
@@ -27,14 +29,23 @@ function App() {
 
 
     return (
-        <>
+        <Router>
         <Header />
         <div className="container">
-            <FeedbackForm handleAdd={addFeedback}/>
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+            <Route exact path="/" element={
+                <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                </>
+            } />
+            <Route exact path="/about" element={<AboutPage/>}/>
+        
+        </Routes>
+            
         </div>
-        </>
+        </Router>
     )
 }
 
